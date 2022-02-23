4b2a15c96cffde335361dbfab5aa7793161752cb
Max <v2asault@yahoo.com.tw>
2022-02-23 23:06:45 +0800

    add create document feature

--- reading-list/src/components/CreateBookForm.vue
+++ reading-list/src/components/CreateBookForm.vue
@@ -3,30 +3,42 @@
     <h3>Add a New Book</h3>
 
     <label for="title">Book title:</label>
-    <input type="text" name="title" v-model="title" required>
+    <input type="text" name="title" v-model="title" required />
 
     <label for="author">Book author:</label>
-    <input type="text" name="author" v-model="author" required>
+    <input type="text" name="author" v-model="author" required />
 
     <button>Add Book</button>
   </form>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from "vue";
+
+import { db } from "../firebase/config";
+import { addDoc, collection } from "firebase/firestore";
 
 export default {
   setup() {
-    const title = ref('')
-    const author = ref('')
+    const title = ref("");
+    const author = ref("");
 
     const handleSubmit = async () => {
-      console.log(title.value, author.value)
-    }
+      const colRef = collection(db, "books");
 
-    return { handleSubmit, title, author }
+      await addDoc(colRef, {
+        title: title.value,
+        author: author.value,
+        isFav: false
+      });
+
+      title.value = "";
+      author.value = "";
+    };
+
+    return { handleSubmit, title, author };
   }
-}
+};
 </script>
 
 <style>
