80738b192f195886c499b4515e9536d14d7276d8
Max <v2asault@yahoo.com.tw>
2022-03-22 23:57:44 +0800

    init project

---
+++ live-chat/src/components/LoginForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="email" required placeholder="email" v-model="email" />
+    <input type="password" required placeholder="password" v-model="password" />
+    <button type="submit">Log in</button>
+  </form>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const email = ref("");
+const password = ref("");
+
+const handleSubmit = () => {
+  console.log(email.value, password.value);
+};
+</script>
+
+<style></style>
---
+++ live-chat/src/views/Welcome.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="welcome container">
+    <p>welcome</p>
+    <div v-if="showLogin">
+      <h2>Login</h2>
+      <LoginForm />
+      <p>
+        No account yet? <span @click="showLogin = false">Signup</span> instead.
+      </p>
+    </div>
+    <div v-else>
+      <h2>Sign up</h2>
+      <SignupForm />
+      <p>
+        Already registered?
+        <span @click="showLogin = true">Login</span> instead.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import LoginForm from "@/components/LoginForm.vue";
+import SignupForm from "@/components/SignupForm.vue";
+
+const showLogin = ref(true);
+</script>
+
+<style lang="scss">
+.welcome {
+  text-align: center;
+  padding: 20px 0;
+
+  form {
+    width: 300px;
+    margin: 20px auto;
+  }
+
+  label {
+    display: block;
+    margin: 20px 0 10px;
+  }
+
+  input {
+    width: 100%;
+    padding: 10px;
+    border-radius: 20px;
+    border: 1px solid #eee;
+    outline: none;
+    color: #999;
+    margin: 10px auto;
+  }
+
+  span {
+    font-weight: bold;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+  button {
+    margin: 20px auto;
+  }
+}
+</style>
